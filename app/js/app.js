e770639a7d9f9105c07ea51b80617984762d9f1b
fanalis <fanalis328@gmail.com>
2018-09-16 23:48:58 +0300

    hleb 1.3

--- app/js/app.js
+++ app/js/app.js
@@ -1,65 +1,25 @@
-'use strict';
+$(document).ready(function() {
+    $('select').niceSelect();
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+     //owl
+    $('.header-slider ').owlCarousel({
+        items: 1,
+        dots: true,
+        smartSpeed: 350,
+        animateOut: 'fadeOut',
+        dotsContainer: '#carousel-custom-dots'
+    });
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+    $( '.owl-dot' ).on( 'click', function() {
+        $('.owl-carousel').trigger('to.owl.carousel', [$(this).index(), 300]);
+        $( '.owl-dot' ).removeClass( 'active' );
+        $(this).addClass( 'active' );
+    });
 
-var VACANCY = function () {
-    function VACANCY() {
-        _classCallCheck(this, VACANCY);
-
-        var _self = this;
-        _self.doc = document;
-        _self.window = window;
-    }
-
-    // Window load types (loading, dom, full)
-
-
-    _createClass(VACANCY, [{
-        key: 'appLoad',
-        value: function appLoad(type, callback) {
-            var _self = this;
-
-            switch (type) {
-                case 'loading':
-                    if (_self.doc.readyState === 'loading') {
-                        callback();
-                    }
-
-                    break;
-                case 'dom':
-                    _self.doc.onreadystatechange = function () {
-                        if (_self.doc.readyState === 'complete') {
-                            callback();
-                        }
-                    };
-
-                    break;
-                case 'full':
-                    _self.window.onload = function (e) {
-                        callback(e);
-                    };
-
-                    break;
-                default:
-                    callback();
-            }
-        }
-    }, {
-        key: 'experienceLogic',
-        value: function experienceLogic() {
-            $('.js-work-experience').change(function (e) {
-                var $target = $(e.target);
-                var $hiddenBlocks = $('.js-visible-no-experience');
-                if ($target.is(':checked') && $target.data('value') === false) {
-                    $hiddenBlocks.removeClass('fw-hidden');
-                } else if ($target.is(':checked') && $target.data('value') === true) {
-                    $hiddenBlocks.addClass('fw-hidden');
-                }
-            });
-        }
-    }]);
-
-    return VACANCY;
-}();
+//     burger menu
+    $('.main-menu-link').on('click', function() {
+        $(".burger-menu").toggleClass("burger-menu--opened");
+        $(".burger-menu").toggleClass("burger-menu--closed");
+        $('.menu').toggleClass('menu_active');
+    });
+});
