40a53a9ed82f620a7057dd45ddf966ca68f70577
fanalis <fanalis328@gmail.com>
2018-09-18 00:54:07 +0300

    hleb 1.4

--- app/js/app.js
+++ app/js/app.js
@@ -18,8 +18,37 @@ $(document).ready(function() {
 
 //     burger menu
     $('.main-menu-link').on('click', function() {
+        $('.fixed-menu').toggleClass('fixed-menu-open');
+        $(this).toggleClass("main-menu-link-active");
         $(".burger-menu").toggleClass("burger-menu--opened");
         $(".burger-menu").toggleClass("burger-menu--closed");
         $('.menu').toggleClass('menu_active');
+        $('.js-fixed-menu-right').removeClass('fixed-menu-right-active');
+        $("ul.fixed-menu-tabs-navigation li").removeClass("active");
     });
+
+
+//    tabs
+
+    $(function () {
+        // $(".fixed-menu-tab-content").hide();
+        // $(".fixed-menu-tab-content:first").show();
+
+        /* if in tab mode */
+        $("ul.fixed-menu-tabs-navigation li").on('click', function() {
+            var activeTab = $(this).attr("rel");
+            $('.js-fixed-menu-right').addClass('fixed-menu-right-active');
+            $('.js-fixed-menu-tab-content').removeClass('fixed-menu-tab-content-active');
+
+            $("#"+activeTab).fadeIn(function () {
+                $(this).addClass('fixed-menu-tab-content-active');
+            });
+
+            $("ul.fixed-menu-tabs-navigation li").removeClass("active");
+            $(this).addClass("active");
+
+        });
+
+    });
+
 });
