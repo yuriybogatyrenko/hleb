671edf884d9d7c974deec9f1f30faf30bf16b852
Yuriy Bogatyrenko <yurabogatyrenko@gmail.com>
2018-09-21 17:03:48 +0600

    stepping

--- app/js/app.js
+++ app/js/app.js
@@ -1,35 +1,63 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // select
     $('select').niceSelect();
 
-     // modal video
+    var $animatedImage = $('.js-first-screen-animated-image');
+
+    if($animatedImage.length) {
+        $animatedImage.each(function () {
+            $(this).addClass('first-screen-animated-image--active')
+        });
+    }
+
+    // modal video
     $(".js-modal-btn").modalVideo();
-     // WOW js
+    // WOW js
     new WOW().init();
 
-     // toggle contacts
-    $('.js-toggle-contacts').on('click', function () {
-       $(this).toggleClass('active');
-       $(this).next().slideToggle();
+    var $windowHeight = $(window).height();
+    var $scrollTopBtn = $('.scroll-top-button');
+    var $headerPlaceholder = $('.header-placeholder');
+    var $header = $('.header');
+
+    $(window).resize(function () {
+        $windowHeight = $(window).height();
+        $headerPlaceholder.css({height: $header.height()});
     });
 
-     // smooth scroll
-    $('.js-go-to').on('click', function(){
+    $(window).trigger('resize');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > $windowHeight) {
+            $scrollTopBtn.addClass('visible');
+        } else {
+            $scrollTopBtn.removeClass('visible');
+        }
+    });
+
+    // toggle contacts
+    $('.js-toggle-contacts').on('click', function () {
+        $(this).toggleClass('active');
+        $(this).next().slideToggle();
+    });
+
+    // smooth scroll
+    $('.js-go-to').on('click', function () {
         var scroll_el = $(this).attr('href');
-        if ($(scroll_el).length != 0){
-            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
+        if ($(scroll_el).length !== 0) {
+            $('html, body').animate({scrollTop: $(scroll_el).offset().top}, 500);
         }
         return false;
     });
-     //owl
+    //owl
 
     $('.js-command-slider').owlCarousel({
         items: 3,
         dots: false,
-        nav:true,
+        nav: true,
         margin: 160,
         smartSpeed: 350,
-        navText: ['<i class="icon-arrow fw-fz-16"></i>','<i class="icon-arrow fw-fz-16"></i>']
+        navText: ['<i class="icon-arrow fw-fz-16"></i>', '<i class="icon-arrow fw-fz-16"></i>']
     });
 
     $('.header-slider ').owlCarousel({
@@ -40,14 +68,14 @@ $(document).ready(function() {
         dotsContainer: '#carousel-custom-dots'
     });
 
-    $( '.owl-dot' ).on( 'click', function() {
+    $('.owl-dot').on('click', function () {
         $('.owl-carousel').trigger('to.owl.carousel', [$(this).index(), 300]);
-        $( '.owl-dot' ).removeClass( 'active' );
-        $(this).addClass( 'active' );
+        $('.owl-dot').removeClass('active');
+        $(this).addClass('active');
     });
 
 //     burger menu
-    $('.main-menu-link').on('click', function() {
+    $('.main-menu-link').on('click', function () {
         $('.fixed-menu').toggleClass('fixed-menu-open');
         $(this).toggleClass("main-menu-link-active");
         $(".burger-menu").toggleClass("burger-menu--opened");
@@ -64,7 +92,7 @@ $(document).ready(function() {
         $('.priorities-wrap-list-item').on('click', function () {
             var activeTab = $(this).attr("rel");
             $('.js-priorities-wrap-tabs-tab').removeClass('active');
-            $("#"+activeTab).fadeIn(function () {
+            $("#" + activeTab).fadeIn(function () {
                 $(this).addClass('active');
             });
 
@@ -72,12 +100,12 @@ $(document).ready(function() {
             $(this).addClass("active");
         });
 
-        $("ul.fixed-menu-tabs-navigation li").on('click', function() {
+        $("ul.fixed-menu-tabs-navigation li").on('click', function () {
             var activeTab = $(this).attr("rel");
             $('.js-fixed-menu-right').addClass('fixed-menu-right-active');
             $('.js-fixed-menu-tab-content').removeClass('fixed-menu-tab-content-active');
 
-            $("#"+activeTab).fadeIn(function () {
+            $("#" + activeTab).fadeIn(function () {
                 $(this).addClass('fixed-menu-tab-content-active');
             });
 
