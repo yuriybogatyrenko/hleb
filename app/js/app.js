883dbb88994c6c1b2fc23f8680fce4402279f474
Yuriy Bogatyrenko <yurabogatyrenko@gmail.com>
2018-09-24 20:45:03 +0600

    stepping

--- app/js/app.js
+++ app/js/app.js
@@ -50,6 +50,8 @@ $(document).ready(function () {
         var scroll_el = $(this).attr('href');
         if ($(scroll_el).length !== 0) {
             $('html, body').animate({scrollTop: $(scroll_el).offset().top}, 500);
+        } else {
+
         }
         return false;
     });
